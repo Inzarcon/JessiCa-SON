a1d2a9617d4650e0388fb5ee58159065ff3ddd85
Inzarcon <136692178+Inzarcon@users.noreply.github.com>
2024-02-28 20:12:16 +0100

    [main.py] Move app paths to main.py

--- main.py
+++ main.py
@@ -1,11 +1,19 @@
-if __name__ == "__main__":
-    import os
-    import sys
+import os
+import sys
+from pathlib import Path
 
-    cur_dir = os.path.dirname(__file__)
-    sys.path.append(os.path.join(cur_dir, "src"))
+# TODO: Option for user to set their own preferred location?
+# TODO: Also consider Qt's resource management. (Though overkill at this stage)
+ROOT_PATH = Path(__file__).resolve().parent
+CFG_PATH = ROOT_PATH.joinpath(".config")
+ICON_PATH = ROOT_PATH.joinpath("resources/icons")
+
+if __name__ == "__main__":
+    sys.path.append(str(ROOT_PATH.joinpath("src")))
     # Local libvips BEFORE other entries to avoid dll conflicts.
-    os.environ["PATH"] = os.path.join(cur_dir, "libvips/bin") + ";" + os.environ["PATH"]
+    os.environ["PATH"] = (
+        str(ROOT_PATH.joinpath("libvips/bin")) + ";" + os.environ["PATH"]
+    )
 
     from main_window import run  # type: ignore
 
