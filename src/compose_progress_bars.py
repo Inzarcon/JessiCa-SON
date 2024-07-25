15532c65268424b238597f59130a377b6c9106a4
Inzarcon <136692178+Inzarcon@users.noreply.github.com>
2024-07-26 00:36:51 +0200

    Rename some variables in ComposeProgressBars

--- src/compose_progress_bars.py
+++ src/compose_progress_bars.py
@@ -16,20 +16,20 @@ class ComposeProgressBars(QWidget):
         """Create ComposeProgressBars."""
         super().__init__()
 
-        self.label_loaded = FormattedQLabel("Sprites loaded:", font_size=10, bold=True)
-        self.progress_loaded = QProgressBar()
-        self._set_color(self.progress_loaded, "#4175c4")
+        self.lbl_loaded = FormattedQLabel("Sprites loaded:", font_size=10, bold=True)
+        self.progress_bar_loaded = QProgressBar()
+        self._set_color(self.progress_bar_loaded, "#4175c4")
 
-        self.label_composed = FormattedQLabel("Sprites composed:", font_size=10, bold=True)
-        self.progress_composed = QProgressBar()
-        self._set_color(self.progress_composed, "#669ff5")
+        self.lbl_composed = FormattedQLabel("Sprites composed:", font_size=10, bold=True)
+        self.progress_bar_composed = QProgressBar()
+        self._set_color(self.progress_bar_composed, "#669ff5")
 
         layout = QGridLayout()
         self.setLayout(layout)
-        layout.addWidget(self.label_loaded, 0, 0)
-        layout.addWidget(self.progress_loaded, 0, 1)
-        layout.addWidget(self.label_composed, 1, 0)
-        layout.addWidget(self.progress_composed, 1, 1)
+        layout.addWidget(self.lbl_loaded, 0, 0)
+        layout.addWidget(self.progress_bar_loaded, 0, 1)
+        layout.addWidget(self.lbl_composed, 1, 0)
+        layout.addWidget(self.progress_bar_composed, 1, 1)
         layout.setContentsMargins(0, 0, 0, 0)
 
         self.reset()
@@ -38,8 +38,8 @@ class ComposeProgressBars(QWidget):
         """Set up the progress bars for the next run based on the given sprite numbers."""
         self._cur_composed = {tilesheet: 0 for tilesheet in sprites_per_tilesheet}
         self._sprites_per_tilesheet = sprites_per_tilesheet
-        self.progress_loaded.setRange(0, total_sprites)
-        self.progress_composed.setRange(0, total_sprites)
+        self.progress_bar_loaded.setRange(0, total_sprites)
+        self.progress_bar_composed.setRange(0, total_sprites)
         self.show()
 
     def set_subset(self, subset: tuple[str]) -> None:
@@ -54,7 +54,7 @@ class ComposeProgressBars(QWidget):
     def update_percent(self, sheet_name: str, percent: int) -> None:
         """Update percentage of sprites composed so far for a specific tilesheet."""
         self._cur_composed[sheet_name] = percent
-        self.progress_composed.setValue(self._calc_sprites_composed())
+        self.progress_bar_composed.setValue(self._calc_sprites_composed())
 
     def _set_color(self, progress_bar: QProgressBar, color: str) -> None:
         css = r"QProgressBar::chunk {background: " + color + "}"
@@ -63,11 +63,11 @@ class ComposeProgressBars(QWidget):
     def increment_loaded(self) -> None:
         """Increment the counter for sprites loaded so far."""
         self._cur_loaded += 1
-        self.progress_loaded.setValue(self._cur_loaded)
+        self.progress_bar_loaded.setValue(self._cur_loaded)
 
     def reset(self) -> None:
         """Reset to initial state."""
         self.hide()
-        self.progress_loaded.setValue(0)
-        self.progress_composed.setValue(0)
+        self.progress_bar_loaded.setValue(0)
+        self.progress_bar_composed.setValue(0)
         self._cur_loaded = 0
